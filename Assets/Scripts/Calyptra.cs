4fd9441cdbd7167be322b98a9e06d63afa642b3e
Baptiste Ottino <baptiste.ottino@mindmaze.ch>
2023-02-04 18:41:15 +0100

    changed calyptra to appear on hover

--- Assets/Scripts/Calyptra.cs
+++ Assets/Scripts/Calyptra.cs
@@ -1,22 +1,61 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Racines
 {
+    [RequireComponent(typeof(SpriteRenderer))]
     public class Calyptra : MonoBehaviour
     {
-        private float _hoverScaleFactor = 1.5f;
+        private const float HoverScaleFactor = 1.5f;
+        private SpriteRenderer _spriteRenderer;
 
         public event Action Clicked = delegate { };
 
+        protected void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            SetSpriteAlpha(0f, 0f);
+        }
+
+        private void SetSpriteAlpha(float alpha, float animationTime)
+        {
+            if (animationTime == 0f)
+            {
+                var color = _spriteRenderer.color;
+                color.a = alpha;
+                _spriteRenderer.color = color;
+                return;
+            }
+            
+            StartCoroutine(AlphaCoroutine(alpha, animationTime));
+        }
+
+        private IEnumerator AlphaCoroutine(float targetAlpha, float animationTime)
+        {
+            var color = _spriteRenderer.color;
+            float initAlpha = color.a;
+            float timeElapsed = 0f;
+            while (timeElapsed < animationTime)
+            {
+                color.a = Mathf.Lerp(initAlpha, targetAlpha, timeElapsed / animationTime);
+                _spriteRenderer.color = color;
+                timeElapsed += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
         private void OnMouseEnter()
         {
-            transform.localScale *= _hoverScaleFactor;
+            SetSpriteAlpha(1f, 0.1f);
+            transform.localScale *= HoverScaleFactor;
         }
 
         private void OnMouseExit()
         {
-            transform.localScale /= _hoverScaleFactor;
+            SetSpriteAlpha(0f, 0.1f);
+            transform.localScale /= HoverScaleFactor;
         }
 
         private void OnMouseDown()
