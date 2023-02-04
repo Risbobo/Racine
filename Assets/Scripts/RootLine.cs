05a6bfa6bf6b9043b2ccb3389c49d75935797c58
Baptiste Ottino <baptiste.ottino@mindmaze.ch>
2023-02-05 00:05:35 +0100

    rebuilt drawing logic with line renderer

---
+++ Assets/Scripts/RootLine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Racines
+{
+    public class RootLine : MonoBehaviour
+    {
+        private LineRenderer _lineRenderer;
+        private Node _node;
+
+        protected void Start()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+            _node = GetComponentInParent<Node>();
+        }
+
+        protected void Update()
+        {
+            var positions = new Vector3[] { _node.GetRootPosition(), _node.GetTipPosition() };
+            _lineRenderer.SetPositions(positions);
+            _lineRenderer.startWidth = _node.Width;
+            _lineRenderer.endWidth = _node.Width;
+        }
+    }
+}
