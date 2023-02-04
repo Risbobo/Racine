f1b382c8bb36a7e4f8ebb771fab7d29f83b0376c
Baptiste Ottino <baptiste.ottino@mindmaze.ch>
2023-02-04 13:49:58 +0100

    Integrated width growing from Boris's branch

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -8,14 +8,19 @@ namespace Racines
     public class Node : MonoBehaviour
     {
         [SerializeField] private int _depth;
+        [SerializeField] private float _initialWidth = 1f;
         [SerializeField] private int _maxDepth = 5;
         [SerializeField] private GameObject _shapePrefab;
-
+        [SerializeField] private float _shrinkFactor;
+        
         private RootParams _rootParams;
+        private Node _parent;
         private bool _isLeaf;
+        [SerializeField] private float _width;
 
         protected void Start()
         {
+            _width = _initialWidth;
             _rootParams = GetComponent<RootParams>();
             Grow();
             _rootParams.Calyptra.Clicked += OnCalyptraClicked;
@@ -25,6 +30,11 @@ namespace Racines
         {
             _maxDepth += 3;
             Grow();
+        }
+
+        private Vector3 GetCalyptraPosition()
+        {
+            return _rootParams.Calyptra.transform.position;
         }
 
         private void Grow()
@@ -44,19 +54,39 @@ namespace Racines
         private IEnumerator CreateChildren()
         {
             yield return new WaitForSeconds(0.5f);
-            Instantiate(_shapePrefab, transform).AddComponent<Node>().Initialize(this);
-            Instantiate(_shapePrefab, transform).AddComponent<Node>().Initialize(this, -1f);
+            Instantiate(_shapePrefab).AddComponent<Node>().Initialize(this);
+            Instantiate(_shapePrefab).AddComponent<Node>().Initialize(this, -1f);
         }
         
-        public void Initialize(Node parent, float factor = 1f)
+        private void Initialize(Node parent, float factor = 1f)
         {
-            transform.parent = parent.transform;
-            transform.localPosition = new Vector3(0f, 1f, 0f);
-            transform.localRotation = Quaternion.AngleAxis(factor * Random.Range(30f, 70f), Vector3.forward);
-            transform.localScale = Random.Range(0.5f, 0.9f) * Vector3.one;
+            _shrinkFactor = parent._shrinkFactor;
+            _parent = parent;
+
+            transform.position = parent.GetCalyptraPosition();
+            transform.rotation = Quaternion.AngleAxis(factor * Random.Range(15f, 75f), Vector3.forward) * parent.transform.rotation;
+            
             _shapePrefab = parent._shapePrefab;
             _maxDepth = parent._maxDepth;
+            _initialWidth = parent._initialWidth;
             _depth = parent._depth + 1;
+            
+            parent.GrowWidth();
+        }
+        
+        private void GrowWidth()
+        {
+            // To Do : Rendu graphique en fonction de la _width
+            var newWidth = Mathf.Sqrt(_width * _width + _initialWidth * _initialWidth);
+            
+            var scale = transform.localScale;
+            transform.localScale = new Vector3((newWidth / _width) * scale.x, scale.y, scale.z);
+            _width = newWidth;
+
+            if (_parent != null)
+            {
+                _parent.GrowWidth();
+            }
         }
     }
 }
