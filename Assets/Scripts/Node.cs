2463f1e8d6069f2371e38acd1c59df7288566976
Baptiste Ottino <baptiste.ottino@mindmaze.ch>
2023-02-04 15:51:31 +0100

    added max angles + global params

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -8,19 +8,18 @@ namespace Racines
     public class Node : MonoBehaviour
     {
         [SerializeField] private int _depth;
-        [SerializeField] private float _initialWidth = 1f;
         [SerializeField] private int _maxDepth = 5;
         [SerializeField] private GameObject _shapePrefab;
-        [SerializeField] private float _shrinkFactor;
         
         private RootParams _rootParams;
         private Node _parent;
         private bool _isLeaf;
+        
         [SerializeField] private float _width;
 
         protected void Start()
         {
-            _width = _initialWidth;
+            _width = RootManager.Instance.initialWidth;
             _rootParams = GetComponent<RootParams>();
             Grow();
             _rootParams.Calyptra.Clicked += OnCalyptraClicked;
@@ -28,7 +27,7 @@ namespace Racines
 
         private void OnCalyptraClicked()
         {
-            _maxDepth += 3;
+            _maxDepth = _depth + RootManager.Instance.depthIncrement;
             Grow();
         }
 
@@ -54,38 +53,39 @@ namespace Racines
         private IEnumerator CreateChildren()
         {
             yield return new WaitForSeconds(0.5f);
-            Instantiate(_shapePrefab).AddComponent<Node>().Initialize(this);
-            Instantiate(_shapePrefab).AddComponent<Node>().Initialize(this, -1f);
+            Instantiate(_shapePrefab).AddComponent<Node>().Initialize(this, GetFanAngle());
+            Instantiate(_shapePrefab).AddComponent<Node>().Initialize(this, -GetFanAngle());
+        }
+
+        private static float GetFanAngle()
+        {
+            return Random.Range(RootManager.Instance.minFanAngle, RootManager.Instance.maxFanAngle);
         }
         
-        private void Initialize(Node parent, float factor = 1f)
+        private void Initialize(Node parent, float angle)
         {
-            _shrinkFactor = parent._shrinkFactor;
             _parent = parent;
 
             transform.position = parent.GetCalyptraPosition();
-            transform.rotation = Quaternion.AngleAxis(factor * Random.Range(15f, 75f), Vector3.forward) * parent.transform.rotation;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward) * parent.transform.rotation;
             
             _shapePrefab = parent._shapePrefab;
             _maxDepth = parent._maxDepth;
-            _initialWidth = parent._initialWidth;
             _depth = parent._depth + 1;
             
-            parent.GrowWidth();
+            parent.Widen();
         }
         
-        private void GrowWidth()
+        private void Widen()
         {
-            // To Do : Rendu graphique en fonction de la _width
-            var newWidth = Mathf.Sqrt(_width * _width + _initialWidth * _initialWidth);
-            
+            var newWidth = Mathf.Sqrt(_width * _width + RootManager.Instance.initialWidth * RootManager.Instance.initialWidth);
             var scale = transform.localScale;
             transform.localScale = new Vector3((newWidth / _width) * scale.x, scale.y, scale.z);
             _width = newWidth;
 
             if (_parent != null)
             {
-                _parent.GrowWidth();
+                _parent.Widen();
             }
         }
     }
---
+++ Assets/Scripts/RootManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Racines
+{
+    public class RootManager : MonoBehaviour
+    {
+        public float minFanAngle;
+        public float maxFanAngle;
+        public int depthIncrement;
+        public float initialWidth;
+
+        private static RootManager _instance;
+
+        public static RootManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = GameObject.FindObjectOfType<RootManager>();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
