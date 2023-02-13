c6ddd3c95c702334312d2a67e2102579d70385cd
Setuhn <setuhn@gmail.com>
2023-02-13 15:59:33 +0100

    Added flower

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -9,6 +9,44 @@ namespace Racines
 {
     public class ObstacleManager : MonoBehaviour
     {
+        private GameManager _gameManager;
+
+        public int obstacleNum = 15;
+
+        void Start()
+        {
+            _gameManager = GameManager.Instance;
+            ObstacleGenerator(obstacleNum);
+        }
+
+        public void ObstacleGenerator(int limitObstacletNum)
+        {
+            int obstacleNumCurrent = 0;
+
+            while (obstacleNumCurrent < limitObstacletNum)
+            {
+                // Determine obstacle size, roation and position
+                float obstacleSize = Random.Range(20f, 100f);
+                float obstacleRotation = Random.Range(0f, 360f);
+                Vector3 obstaclePosition = _gameManager.GetRandomGroundPosition(new Vector2(obstacleSize, obstacleSize));
+
+                //Rotate(Vector3.forward, obstacleRotation);
+
+                //// Check for collision with other collider objects (rocks, other nutrient, etc)
+                //if (Physics2D.OverlapCircle(nutrientPosition, 2 * nutrientSize) == null)
+                //{
+                //    // Create new Nutrient
+                //    Nutrient newNutrient = Instantiate(_nutrientPrefab, parent: transform);
+
+                //    newNutrient.Initialize(nutrientPosition, nutrientValue, nutrientSize, _nutrientAbsorbFactor);
+
+                //    // Add to the nutrient list
+                //    _gameManager.nutrientList.Add(newNutrient);
+
+                //    nutrientNumCurrent += 1;
+                //}
+            }
+        }
 
     }
 }
